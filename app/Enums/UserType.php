b9907d03a7764edb449709e39f1f43390f079ca1
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2024-08-31 00:39:27 -0300

    feature: crud de users no painel de admin

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -6,4 +6,12 @@ enum UserType: string
 {
     case user = 'user';
     case admin = 'admin';
+
+    public static function getOptions(): array
+    {
+        return [
+            self::admin->value => 'Admin',
+            self::user->value => 'User',
+        ];
+    }
 }
